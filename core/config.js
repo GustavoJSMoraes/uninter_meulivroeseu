a2c05f4456fbd37e3b33a84625eccb800c96e12e
GustavoJSMoraes <gustavo.jsm@hotmail.com>
2026-06-15 19:12:53 -0300

    Add files via upload

---
+++ core/config.js
@@ -0,0 +1,25 @@
+export const route = 'partials/'
+export const container = '#container-content'
+export const container_acervo = '#div-acervo'
+export const logado = {
+    id: 0,
+    nome: '',
+    primeiro_nome: '',
+}
+
+export const materias = [
+    'Outros...',
+    'Arte',
+    'Biologia',
+    'Espanhol',
+    'Filososfia',
+    'Física',
+    'Geografia',
+    'História',
+    'Inglês',
+    'Língua portuguesa',
+    'Literatura',
+    'Matemática',
+    'Química',
+    'Sociologia'
+]
